a8ece8387a89a6d86603651c0692996dfc114b40
William Cotton <wcotton229@gmail.com>
2018-10-28 21:56:42 -0500

    Routes are working and signup/login pages are working

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,20 +7,56 @@ var path = require("path");
 module.exports = function(app) {
 
   app.get("/", function(req, res) {
-    // If the user already has an account send them to the landing page
-    // if (req.user) {
-    //   res.redirect("/");
-    // }
-    // res.sendFile(path.join(__dirname, "../signup.html"));
+    res.sendFile(path.join(__dirname, "../index.html"));
   });
 
-  // app.get("/login", function(req, res) {
-  //   // If the user already has an account send them to the landing page
-  //   if (req.user) {
-  //     res.redirect("/");
-  //   }
-  //   res.sendFile(path.join(__dirname, "../login.html"));
-  // });
+  app.get("/electric", function(req, res) {
+    res.sendFile(path.join(__dirname, "../electric.html"));
+  });
+
+  app.get("/network", function(req, res) {
+    res.sendFile(path.join(__dirname, "../network.html"));
+  });
+
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../about.html"));
+  });
+
+  app.get("/chargingguide", function(req, res) {
+    res.sendFile(path.join(__dirname, "../charginggudie.html"));
+  });
+
+  app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "../contact.html"));
+  });
+
+  app.get("/faq", function(req, res) {
+    res.sendFile(path.join(__dirname, "../faq.html"));
+  });
+
+  app.get("/invest", function(req, res) {
+    res.sendFile(path.join(__dirname, "../invest.html"));
+  });
+
+  app.get("/pricing", function(req, res) {
+    res.sendFile(path.join(__dirname, "../pricing.html"));
+  });
+
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the landing page
+    if (req.user) {
+      res.redirect("../");
+    }
+    res.sendFile(path.join(__dirname, "../signup.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the landing page
+    if (req.user) {
+      res.redirect("../");
+    }
+    res.sendFile(path.join(__dirname, "../login.html"));
+  });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
